3be842c2a257b27aa63093728d67716fa527ee28
Pablo Papalardo <pablopapalardo2@gmail.com>
2021-12-25 11:57:09 -0300

    Update PancakeOracleLibrary.sol

--- contracts/libraries/PancakeOracleLibrary.sol
+++ contracts/libraries/PancakeOracleLibrary.sol
@@ -17,11 +17,11 @@ library PancakeOracleLibrary {
         address pair
     ) internal view returns (uint price0Cumulative, uint price1Cumulative, uint32 blockTimestamp) {
         blockTimestamp = currentBlockTimestamp();
-        price0Cumulative = IPancakePair(pair).price0CumulativeLast();
-        price1Cumulative = IPancakePair(pair).price1CumulativeLast();
+        price0Cumulative = IUniswapV2Pair(pair).price0CumulativeLast();
+        price1Cumulative = IUniswapV2Pair(pair).price1CumulativeLast();
 
         // if time has elapsed since the last update on the pair, mock the accumulated price values
-        (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = IPancakePair(pair).getReserves();
+        (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = IUniswapV2Pair(pair).getReserves();
         if (blockTimestampLast != blockTimestamp) {
             // subtraction overflow is desired
             uint32 timeElapsed = blockTimestamp - blockTimestampLast;
